8c44578932a16f1dc210da5e4bc74f223f0ce197
gabeparra <ma.parrar16@gmail.com>
2023-06-19 21:40:02 -0400

    owo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the CSS for the app. You need to create this file.
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Gabriel Parra's Portfolio</h1>
+        <h2>Computer Science Student & IT Graduate</h2>
       </header>
+
+      <section className="App-intro">
+        <h2>About Me</h2>
+        <p>
+          Hi, I'm Gabriel, a Computer Science student and IT graduate. I have a passion for solving problems and creating solutions through coding and technology.
+        </p>
+      </section>
+
+      <section className="App-projects">
+        <h2>Projects</h2>
+        <p>Stay tuned for my exciting projects!</p>
+      </section>
+
+      <footer className="App-footer">
+        <p>Connect with me on LinkedIn and GitHub.</p>
+      </footer>
     </div>
   );
 }
